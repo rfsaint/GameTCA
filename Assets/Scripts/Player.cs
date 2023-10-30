6ff35f38a5d9d1836d8cf6bf011f54305b646cb4
Rone Augusto da Silva <rm91874@estudante.fieb.edu.br>
2023-10-30 14:17:35 -0300

    Versão Inicial

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int _lives = 5;
     public float Veloc;
     private float entradaHorizontal;
     private float entradaVertical;
     [SerializeField]
     private float JumpForce = 10.0f;
-    public bool isGrounded = true;
+    public bool isGrounded;
     [SerializeField]
     private Rigidbody2D rig;
     // Start is called before the first frame update
@@ -28,7 +29,7 @@ public class Player : MonoBehaviour
             {
                 // Aplica uma força vertical para fazer o jogador saltar
                 rig.AddForce(Vector3.up * JumpForce, ForceMode2D.Impulse);
-                // O jogador não está mais no chão após o salto
+                isGrounded = !isGrounded; // O jogador não está mais no chão após o salto
             }
         }
         Movimento();
@@ -37,19 +38,26 @@ public class Player : MonoBehaviour
     {
         entradaHorizontal = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * Veloc * Time.deltaTime * entradaHorizontal);
-
-        entradaVertical = Input.GetAxis("Vertical");
-        transform.Translate(Vector2.up * Veloc * Time.deltaTime * entradaVertical);
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        while (true)
-        {
+        
+        
             if (collision.gameObject.CompareTag("Piso"))
             {
                 isGrounded = true;  // O jogador está no chão
             }
-        }
+            else 
+            {
+                isGrounded = false;
+                StartCoroutine(UpTime());
+            }
+    }
+    public IEnumerator UpTime()
+    {
+       yield return new WaitForSeconds(0.2f);
+       isGrounded = true;
+        
     }
 }
